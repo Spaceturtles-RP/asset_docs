b6b9b6d278ec6c47b3304a1608862021ae46cb4d
Aaron <aaron.otaku@gmail.com>
2025-10-22 01:23:00 +0100

    MLO page updates

--- mdx-components.js
+++ mdx-components.js
@@ -1,9 +1,9 @@
 import YoutubeEmbed from './components/youtube-embed'
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs' // nextra-theme-blog or your custom theme
- 
+
 // Get the default MDX components
 const themeComponents = getThemeComponents()
- 
+
 // Merge components
 export function useMDXComponents(components) {
   return {
